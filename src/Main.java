bb50c3fbb70b9b1c0a15624f92d1d7ea7127555f
Fransr98 <47338397+Fransr98@users.noreply.github.com>
2022-04-19 19:14:14 +0100

    Feature : Añadido metodo ToString + Fix : Readme.md

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SpaceShip millenniumfalcon = new SpaceShip("Millenium Falcon","YT-1300 492727ZED",1,
+                1,1,0,0,1,1);
+
+
+        System.out.println(millenniumfalcon.toString());
+
+
+
     }
 }
